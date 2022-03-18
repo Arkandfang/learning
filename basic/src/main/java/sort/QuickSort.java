db36efb0308443392454097b0d26c2f83f04f6be
liufangzhou <liufangzhou@didiglobal.com>
2022-03-18 21:40:45 +0800

    1

---
+++ basic/src/main/java/sort/QuickSort.java
@@ -0,0 +1,11 @@
+package sort;
+
+/**
+ * @author fangzhou
+ * @date 2022-03-18 21:37
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
